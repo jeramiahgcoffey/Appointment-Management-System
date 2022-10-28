2233232c09646a7dba77c5cffc2fe833c0529340
Jeramiah Coffey <jcoffey@rategenius.com>
2022-10-28 14:15:34 -0500

    Setup login ui

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
 /** Main class for the application. */
 public class Main extends Application {
-
     /**
      * Main function which gets called when the application is run.
      *
@@ -25,10 +25,11 @@ public class Main extends Application {
      * @param stage
      */
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/FirstScene.fxml")));
-        stage.setTitle("First Scene");
-        stage.setScene(new Scene(root, 800, 600));
+    public void start(@NotNull Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/Login.fxml")));
+        stage.setTitle("Appointment Management System");
+        stage.setScene(new Scene(root, 405, 250));
+        stage.setResizable(false);
         stage.show();
     }
 }
--- src/controller/Login.java
+++ src/controller/Login.java
@@ -5,7 +5,7 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class FirstScene implements Initializable {
+public class Login implements Initializable {
     /**
      * @param url
      * @param resourceBundle
