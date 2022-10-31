18303dc64b6e894d94c430491284ca3358e4b5ba
Jeramiah Coffey <jcoffey@rategenius.com>
2022-10-31 15:05:11 -0500

    Display customers

--- src/controller/Customers.java
+++ src/controller/Customers.java
@@ -1,27 +1,74 @@
 package controller;
 
 import db.CustomerDAO;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.Customer;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class Customers implements Initializable {
+
+    @FXML
+    private TableView<Customer> custTable;
+
+    @FXML
+    private TableColumn<Number, Integer> custIdCol;
+
+    @FXML
+    private TableColumn<Character, String> nameCol;
+
+    @FXML
+    private TableColumn<Character, String> addressCol;
+
+    @FXML
+    private TableColumn<Character, String> postalCol;
+
+    @FXML
+    private TableColumn<Character, String> stateCol;
+
+    @FXML
+    private TableColumn<Date, Date> createdAtCol;
+
+    @FXML
+    private TableColumn<Character, String> createdByCol;
+
+    @FXML
+    private TableColumn<Date, Date> updatedAtCol;
+
+    @FXML
+    private TableColumn<Character, String> updatedByCol;
+
     /**
      * @param url            URL used to resolve paths, null if not known
      * @param resourceBundle Resources used to localize the root object, null if not localized
      */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.print(CustomerDAO.getInstance().getAll());
+        custIdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        addressCol.setCellValueFactory(new PropertyValueFactory<>("address"));
+        postalCol.setCellValueFactory(new PropertyValueFactory<>("postal"));
+//        stateCol.setCellValueFactory(new PropertyValueFactory<>("state"));
+        createdAtCol.setCellValueFactory(new PropertyValueFactory<>("createdAt"));
+        createdByCol.setCellValueFactory(new PropertyValueFactory<>("createdBy"));
+        updatedAtCol.setCellValueFactory(new PropertyValueFactory<>("updatedAt"));
+        updatedByCol.setCellValueFactory(new PropertyValueFactory<>("updatedBy"));
+        custTable.setItems(FXCollections.observableList(CustomerDAO.getInstance().getAll()));
     }
 
     /**
